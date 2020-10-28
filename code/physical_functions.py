680b70c8622b84271e10078ff7eb14818db86542
Benjamin Habicht <benjaminhabicht@gmx.net>
2020-10-28 20:29:26 +0100

    add definition of gravforce and gravpot

---
+++ code/physical_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.constants as const
+
+
+def gravforce(x1,x2,m1,m2):
+    """
+    This function returns the value of the gravitational force, in SI units, 
+    acting on the first object and caused by the second object, using
+    newton's law of gravitation and newton's 2nd law of motion. I.e. we ignore
+    relativistic effects (relevant for the apsidal precession of e.g. mercury).
+    Input values are the positions of the 2 object in cartesian coordinates and
+    the masses of the objects.
+    """
+    return -const.G*m1*m2/np.linalg.norm(x1-x2)**3*(x1-x2)
+
+def gravpot(x1,x2,m1,m2):
+    return -const.G*m1*m2/np.linalg.norm(x1-x2)
