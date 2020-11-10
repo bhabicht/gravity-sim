08b154ccecfe66b484efcee98ded6e3193e91394
Benjamin Habicht <benjaminhabicht@gmx.net>
2020-11-10 15:59:54 -0300

    add tests for physical_functions.py

--- Code/physical_functions.py
+++ Code/physical_functions.py
@@ -3,15 +3,18 @@ import scipy.constants as const
 
 
 def gravforce(x1,x2,m1,m2):
-    """
-    This function returns the value of the gravitational force, in SI units, 
+    """This function returns the value of the gravitational force, in SI units, 
     acting on the first object and caused by the second object, using
     newton's law of gravitation and newton's 2nd law of motion. I.e. we ignore
     relativistic effects (relevant for the apsidal precession of e.g. mercury).
     Input values are the positions of the 2 object in cartesian coordinates and
     the masses of the objects.
     """
+    if np.array_equal(x1, x2):
+        raise ValueError("Division by 0 Error")
     return -const.G*m1*m2/np.linalg.norm(x1-x2)**3*(x1-x2)
 
 def gravpot(x1,x2,m1,m2):
+    if np.array_equal(x1, x2):
+        raise ValueError("Division by 0 Error")
     return -const.G*m1*m2/np.linalg.norm(x1-x2)
--- tests/physical_functions_test.py
+++ tests/physical_functions_test.py
@@ -4,9 +4,32 @@ import sys
 sys.path.append('/home/benjamin/Documents/computer_science/gravity-sim')
 
 from Code.physical_functions import gravpot
+from Code.physical_functions import gravforce
 
-class PotentialEnergyTestCase(unittest.TestCase):
+class TestGravpot(unittest.TestCase):
+    def test_gravpot_success(self):
+        actual = gravpot(0, 1, 1, 1)
+        expected = -const.G
+        self.assertEqual(actual, expected)
 
-    def test_grav(self):
-        result = gravpot(0, 1, 1, 1)
-        self.assertEqual(result, -const.G)
+    def test_gravpot_exception(self):
+        with self.assertRaises(ValueError) as exception_context:
+            gravpot(0, 0, 1, 1)
+        self.assertEqual(
+            str(exception_context.exception),
+            "Division by 0 Error"
+        )
+
+class TestGravforce(unittest.TestCase):
+    def test_gravforce_success(self):
+        actual = gravforce(0, 1, 1, 1)
+        expected = const.G
+        self.assertEqual(actual, expected)
+
+    def test_gravforce_exception(self):
+        with self.assertRaises(ValueError) as exception_context:
+            gravpot(0, 0, 1, 1)
+        self.assertEqual(
+            str(exception_context.exception),
+            "Division by 0 Error"
+        )
