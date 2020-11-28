caf6311f0385ad86f226a50e4c2cfa0f645ddfa9
Benjamin Habicht <benjaminhabicht@gmx.net>
2020-11-28 04:36:17 -0300

    add test for update_all_velocities

--- tests/leap_frog_algorithm_test.py
+++ tests/leap_frog_algorithm_test.py
@@ -5,7 +5,7 @@ sys.path.append('/home/benjamin/Documents/computer_science/gravity-sim')
 
 import scipy.constants as const
 
-from Code.leap_frog_algorithm import update_all_positions, calculate_all_forces
+from Code.leap_frog_algorithm import update_all_positions, calculate_all_forces, update_all_velocities
 from Code.planet_system_creation import Massiveobject
 
 class TestUpdateAllPositions(unittest.TestCase):
@@ -30,3 +30,16 @@ class TestCalculateAllForces(unittest.TestCase):
         self.assertTrue(np.array_equal(self.obj1.F, np.array([11/4*const.G,0,0])))
         self.assertTrue(np.array_equal(self.obj2.F, np.array([4*const.G,0,0])))
         self.assertTrue(np.array_equal(self.obj3.F, np.array([-27/4*const.G,0,0])))
+
+class TestUpdateAllVelocities(unittest.TestCase):
+    obj1 = Massiveobject("obj1", 1, np.array([0,0,0]), np.array([3,0,0]), np.array([10,0,0]))
+    obj2 = Massiveobject("obj2", 2, np.array([1,0,0]), np.array([4,0,0]), np.array([30,0,0]))
+    obj3 = Massiveobject("obj3", 3, np.array([2,0,0]), np.array([5,0,0]), np.array([30,0,0]))
+    def test_update_all_velocities(self):
+        self.assertTrue(np.array_equal(self.obj1.v, np.array([3,0,0])))
+        self.assertTrue(np.array_equal(self.obj2.v, np.array([4,0,0])))
+        self.assertTrue(np.array_equal(self.obj3.v, np.array([5,0,0])))
+        update_all_velocities(0.5, [self.obj1, self.obj2, self.obj3])
+        self.assertTrue(np.array_equal(self.obj1.v, np.array([8,0,0])))
+        self.assertTrue(np.array_equal(self.obj2.v, np.array([11.5,0,0])))
+        self.assertTrue(np.array_equal(self.obj3.v, np.array([10,0,0])))
