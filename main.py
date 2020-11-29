6138d287ff6a0963a3ed8d250f91fe529690a34c
Benjamin Habicht <benjaminhabicht@gmx.net>
2020-11-29 18:41:08 +0100

    PEP 8 improvements

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from Code.leap_frog_algorithm import leapfrog_nsteps
 from Code.planet_system_creation import sun, mercury, venus, earth, mars
 
 
-delta_t = 60*60*24 #a day in seconds
+delta_t = 60*60*24  # a day in seconds
 selected_objects = [sun, mercury, venus, earth, mars]
 
-nsteps=100 #simulated time interval: nsteps*delta_t = 100 days
+nsteps = 100  # simulated time interval: nsteps*delta_t = 100 days
 '''
 initialize the vectors that will store position and velocities of the objects
 the first index stores the values as function of the time, the second as a
 function of the object and the third stores the x,y and z component
 '''
-x = np.zeros((nsteps,len(selected_objects),3))
-v = np.zeros((nsteps,len(selected_objects),3))
+x = np.zeros((nsteps, len(selected_objects), 3))
+v = np.zeros((nsteps, len(selected_objects), 3))
 
 # now do all the calculation and plotting
 leapfrog_nsteps(nsteps, delta_t, selected_objects, x, v)
